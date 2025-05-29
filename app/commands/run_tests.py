dbff9b0b7189d0165730064776dfc643c5824770
Romulo Quidute Filho <116586593+rquidute@users.noreply.github.com>
2025-05-29 09:36:51 -0300

    Add --tests-list argument to run-tests CLI command (#18)
    * Allow CLI to receive test case as a list
    
    * Update app/commands/run_tests.py
    
    Co-authored-by: Carolina Lopes <116589288+ccruzagralopes@users.noreply.github.com>
    
    * code review
    
    ---------
    
    Co-authored-by: Carolina Lopes <116589288+ccruzagralopes@users.noreply.github.com>

--- app/commands/run_tests.py
+++ app/commands/run_tests.py
@@ -35,14 +35,19 @@ test_collections_api = async_apis.test_collections_api
 def build_test_selection(test_collections, tests_list) -> dict:
     """Build the test selection JSON structure from test_collections and tests_list.
     Example:
-        tests_list = ["TC-ACE-1.1", "TC_ACE_1_3"]
-        test_collections = {
-            "SDK YAML Tests": {
-                "FirstChipToolSuite": {
+        tests_list = "TC-ACE-1.1,TC_ACE_1_3"
+        Selected tests: {
+                "SDK YAML Tests": {
+                    "FirstChipToolSuite": {
                     "TC-ACE-1.1": 1
+                    }
+                },
+                "SDK Python Tests": {
+                    "Python Testing Suite": {
+                    "TC_ACE_1_3": 1
+                    }
                 }
             }
-        }
     """
     selected_tests = {}
 
@@ -93,7 +98,7 @@ def build_test_selection(test_collections, tests_list) -> dict:
 )
 @click.option(
     "--tests-list",
-    help="List of test cases to execute. For example: TC-ACE-1.1,TC_ACE_1_3",
+    help="List of test cases to execute. Separated by commas (,) and without any blank spaces. For example: TC-ACE-1.1,TC_ACE_1_3",
 )
 @async_cmd
 async def run_tests(selected_tests: str, title: str, file: str, project_id: int, tests_list: str = None) -> None:
