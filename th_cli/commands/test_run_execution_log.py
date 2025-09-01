9e423b12266e43d5f84e4c4001dee7befb165d9f
Romulo Quidute Filho <rquidute@apple.com>
2025-09-01 18:01:36 +0000

    Code review

--- th_cli/commands/test_run_execution_log.py
+++ th_cli/commands/test_run_execution_log.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from contextlib import closing
 from typing import Optional
 
 import click
