b430a12becde301f8ac41754e171f8ea47eec9f2
antonio-amjr <116589331+antonio-amjr@users.noreply.github.com>
2025-12-16 12:14:48 -0300

    [Feature] Adding the Node ID to CLI Output (#48)
    * Adding the Node ID to CLI output
    
    * Updating ChipServerInfo attribute type
    
    Plus minor fixes

--- th_cli/commands/run_tests.py
+++ th_cli/commands/run_tests.py
@@ -217,15 +217,24 @@ async def __create_new_test_run_cli(
 async def __start_test_run(
     async_apis: AsyncApis, test_run: m.TestRunExecutionWithChildren
 ) -> m.TestRunExecutionWithChildren:
+    test_run_executions_api = async_apis.test_run_executions_api
     header = colorize_header("Starting Test run")
     title = colorize_key_value("Title", test_run.title)
     id = colorize_key_value("ID", str(test_run.id))
 
     click.echo("")
-    click.echo(f"{header}:\n- {title}\n- {id}\n")
+    click.echo(f"{header}:\n- {title}\n- {id}")
+
+    # Fetch and display ChipServer node ID information
+    try:
+        chip_info = await test_run_executions_api.get_chip_server_info_api_v1_test_run_executions_chip_server_info_get()
+        node_id_hex = colorize_key_value("Node ID", chip_info.node_id_hex)
+        click.echo(f"- {node_id_hex}\n")
+    except UnexpectedResponse as e:
+        # If we can't fetch node_id, don't fail - just skip displaying it
+        click.echo(f"Could not fetch ChipServer node ID information: {e}\n")
 
     try:
-        test_run_executions_api = async_apis.test_run_executions_api
         return await test_run_executions_api.start_test_run_execution_api_v1_test_run_executions_id_start_post(
             id=test_run.id
         )
