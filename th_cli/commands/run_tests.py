2fa98b7bdb2fa085a118361760d59781f8b9ea65
Steven Green <steven.green@smartthings.com>
2026-07-16 15:57:28 -0700

    Re-enable linting action
    Also changes install script to exclude dev dependencies.

--- th_cli/commands/run_tests.py
+++ th_cli/commands/run_tests.py
@@ -42,7 +42,14 @@ from th_cli.config import config as th_config
 from th_cli.exceptions import CLIError, handle_api_error
 from th_cli.test_run.camera.two_way_talk_handler import TwoWayTalkHandler
 from th_cli.test_run.websocket import TestRunSocket
-from th_cli.utils import DEFAULT_CLI_PROJECT_NAME, build_test_selection, convert_nested_to_dict, load_json_config, merge_configs, read_pics_config
+from th_cli.utils import (
+    DEFAULT_CLI_PROJECT_NAME,
+    build_test_selection,
+    convert_nested_to_dict,
+    load_json_config,
+    merge_configs,
+    read_pics_config,
+)
 from th_cli.validation import validate_directory_path, validate_file_path, validate_test_ids
 
 # Constants
@@ -137,8 +144,7 @@ async def run_tests(
         config = str(config_path)
 
     if pics_config_folder:
-        pics_path = validate_directory_path(pics_config_folder, must_exist=True)
-        pics_config_folder = str(pics_path)
+        pics_config_folder = validate_directory_path(pics_config_folder, must_exist=True)
 
     client = None
     _webrtc_handler = None
